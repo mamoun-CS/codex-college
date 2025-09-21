11cfa609282558d45b5469ef921e9e0eeb849649
Mamoun Khanfa <mamounkhanfa5@gmail.com>
2025-09-21 04:22:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
       'fitting-singularly-heron.ngrok-free.app',
       'localhost',
       '127.0.0.1',
+      '10.229.120.84',
       '0.0.0.0'
     ],
   },
@@ -39,6 +40,7 @@ export default defineConfig({
       'fitting-singularly-heron.ngrok-free.app',
       'localhost',
       '127.0.0.1',
+      '10.229.120.84',
       '0.0.0.0'
     ],
   }
